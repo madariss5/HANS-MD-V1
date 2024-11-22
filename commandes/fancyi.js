c7e3420737dc7c06463637a19c203533c7785984
FELIXTZ <hanstzgamestech@gmail.com>
2024-11-23 01:31:36 +0300

    Update fancyi.js

--- commandes/fancyi.js
+++ commandes/fancyi.js
@@ -1,162 +1,160 @@
+require("dotenv").config();
 const {
   zokou
 } = require("../framework/zokou");
 const yts = require("yt-search");
+const BaseUrl = process.env.GITHUB_GIT;
+const giftedapikey = process.env.BOT_OWNER;
+function validateConfig() {
+  if (!BaseUrl || !giftedapikey) {
+    throw new Error("Configuration error: Missing BaseUrl or API key.");
+  }
+}
+validateConfig();
 zokou({
   'nomCom': "video",
   'categorie': "Search",
-  'reaction': '🤳'
-}, async (_0x3643ec, _0x1895de, _0x4b749f) => {
+  'reaction': '📽️'
+}, async (_0x45a4f3, _0x5ad5e6, _0x2d4e5a) => {
   const {
-    ms: _0x34f844,
-    repondre: _0x12e5f8,
-    arg: _0x151977
-  } = _0x4b749f;
-  if (!_0x151977[0]) {
-    _0x12e5f8("Please insert a song/video name.");
-    return;
+    ms: _0x4b19c5,
+    repondre: _0x6cac1b,
+    arg: _0x42bd0a
+  } = _0x2d4e5a;
+  if (!_0x42bd0a[0]) {
+    return _0x6cac1b("𝘱𝘭𝘢𝘴𝘦 𝘪𝘯𝘴𝘦𝘳𝘳 𝘢 𝘴𝘰𝘯𝘨 𝘰𝘳 𝘷𝘪𝘥𝘦𝘰 𝘯𝘢𝘮𝘦.");
   }
   try {
-    let _0x3e8be9 = _0x151977.join(" ");
-    let _0x52a93a = [];
-    const _0xc3a63f = await yts(_0x3e8be9);
-    _0x52a93a = _0xc3a63f.videos;
-    if (_0x52a93a && _0x52a93a.length > 0) {
-      const _0x3a755b = await _0x2bc96f.json();
-      if (_0x3a755b.status === 200 && _0x3a755b.success) {
-        const _0x49df33 = _0x3a755b.result.download_url;
-        const _0x53fc88 = {
-          'image': {
-            'url': _0x52a93a[0].thumbnail
-          },
-          'caption': "HANS-MD-MUC-DOWNLODER\n\n © hanstech"
-        };
-        await _0x1895de.sendMessage(_0x3643ec, _0x53fc88, {
-          'quoted': _0x34f844
-        });
-        await _0x1895de.sendMessage(_0x3643ec, {
-          'video': {
-            'url': _0x49df33
-          },
-          'mimetype': "video/mp4"
-        }, {
-          'quoted': _0x34f844
-        });
-        _0x12e5f8("Downloded Successfully ✅");
-      } else {
-        _0x12e5f8("Searching...🌏");
-      }
-    } else {
-      _0x12e5f8("No videos found.");
+    const _0x56c90e = await yts(_0x42bd0a.join(" "));
+    const _0x3fe8f7 = _0x56c90e.videos;
+    if (_0x3fe8f7.length === 0) {
+      return _0x6cac1b("𝘕𝘰 𝘷𝘪𝘥𝘦𝘰𝘴 𝘧𝘰𝘶𝘯𝘥.");
     }
-  } catch (_0x314c47) {
-    console.error("Error from API:", _0x314c47);
-    _0x12e5f8("Searching...🌏");
-  }
-});
-zokou({
-  'nomCom': "song",
-  'categorie': "Download",
-  'reaction': '🎺'
-}, async (_0x271a33, _0x6707e5, _0x520fff) => {
-  const {
-    ms: _0x1e2f77,
-    repondre: _0x300732,
-    arg: _0x53d21a
-  } = _0x520fff;
-  if (!_0x53d21a[0]) {
-    _0x300732("Please insert a song name.");
-    return;
-  }
-  try {
-    let _0x226ad0 = _0x53d21a.join(" ");
-    let _0x994c29 = [];
-    const _0x9d0929 = await yts(_0x226ad0);
-    _0x994c29 = _0x9d0929.videos;
-    if (_0x994c29 && _0x994c29.length > 0) {
-      const _0x20c972 = await _0x4983e8.json();
-      if (_0x20c972.status === 200 && _0x20c972.success) {
-        const _0x30773c = _0x20c972.result.download_url;
-        const _0x139808 = {
-          'image': {
-            'url': _0x994c29[0].thumbnail
-          },
-          'caption': "HANS-MD-MUC-DOWNLODER\n\n © HANS TECH"
-        };
-        await _0x6707e5.sendMessage(_0x271a33, _0x139808, {
-          'quoted': _0x1e2f77
-        });
-        await _0x6707e5.sendMessage(_0x271a33, {
-          'audio': {
-            'url': _0x30773c
-          },
-          'mimetype': "audio/mp4"
-        }, {
-          'quoted': _0x1e2f77
-        });
-        _0x300732("*Downloded Successfully ✅*");
-      } else {
-        _0x300732("Failed to download audio. Please try again later.");
-      }
+    const _0x27926e = _0x3fe8f7[0].url;
+    const _0xcc10e1 = await fetch(BaseUrl + "/api/download/ytmp4?url=" + encodeURIComponent(_0x27926e) + "&apikey=" + giftedapikey);
+    const _0x87d3a7 = await _0xcc10e1.json();
+    if (_0x87d3a7.status === 200 && _0x87d3a7.success) {
+      const _0x414793 = _0x87d3a7.result.download_url;
+      await _0x5ad5e6.sendMessage(_0x45a4f3, {
+        'image': {
+          'url': _0x3fe8f7[0].thumbnail
+        },
+        'caption': "╔═━━━━━════━━━━─➳\n║ ╔ *𝘿𝙊𝙒𝙉𝙇𝙊𝘿𝙀𝙍* 』\n║ *𝘽𝙤𝙩 𝙣𝙖𝙢𝙚 ; 𝙃𝘼𝙉𝙎-𝙈𝘿 2025* \n║ *𝙊𝙬𝙣𝙚𝙧; 𝙃𝘼𝙉𝙎𝙏𝙕* \n╚══━━━━════━━━━─➳"
+      }, {
+        'quoted': _0x4b19c5
+      });
+      await _0x5ad5e6.sendMessage(_0x45a4f3, {
+        'video': {
+          'url': _0x414793
+        },
+        'mimetype': "video/mp4"
+      }, {
+        'quoted': _0x4b19c5
+      });
+      _0x6cac1b("𝙔𝙤𝙪𝙧-𝙨𝙤𝙣𝙜-𝙙𝙤𝙬𝙣𝙡𝙤𝙖𝙙-𝙨𝙪𝙘𝙘𝙚𝙨𝙨𝙛𝙪𝙡𝙡𝙮");
     } else {
-      _0x300732("No audio found.");
+      _0x6cac1b("Failed to download the video.");
     }
-  } catch (_0x2144bd) {
-    console.error("Error from API:", _0x2144bd);
-    _0x300732("An error occurred while searching or downloading the audio.");
+  } catch (_0x5e93ed) {
+    console.error("Error:", _0x5e93ed);
+    _0x6cac1b("An error occurred while processing your request.");
   }
 });
 zokou({
   'nomCom': "play",
   'categorie': "Download",
-  'reaction': '🎤'
-}, async (_0x80c678, _0x1dc8a0, _0x27d06f) => {
+  'reaction': '🎧'
+}, async (_0x1cb756, _0x49043b, _0x4bb0ff) => {
   const {
-    ms: _0xfb7714,
-    repondre: _0x19492f,
-    arg: _0x50635c
-  } = _0x27d06f;
-  if (!_0x50635c[0]) {
-    _0x19492f("Please insert a song name.");
-    return;
+    ms: _0x999bbc,
+    repondre: _0x47720b,
+    arg: _0x538ab4
+  } = _0x4bb0ff;
+  if (!_0x538ab4[0]) {
+    return _0x47720b("𝙋𝙡𝙖𝙨𝙚 𝙚𝙣𝙩𝙚𝙧 𝙮𝙤𝙪𝙧 𝙨𝙤𝙣𝙜 𝙣𝙖𝙢𝙚.");
   }
   try {
-    let _0x3e9ef3 = _0x50635c.join(" ");
-    let _0x43a4ee = [];
-    const _0x4a30bb = await yts(_0x3e9ef3);
-    _0x43a4ee = _0x4a30bb.videos;
-    if (_0x43a4ee && _0x43a4ee.length > 0) {
-      const _0x4b00b3 = _0x43a4ee[0].url;
-      const _0x2a1daf = await fetch("https://apis.ibrahimadams.us.kg/api/download/ytmp3?url=" + encodeURIComponent(_0x4b00b3) + "&apikey=" + "cracker");
-      const _0x475016 = await _0x2a1daf.json();
-      if (_0x475016.status === 200 && _0x475016.success) {
-        const _0x26f57c = _0x475016.result.download_url;
-        const _0x232b70 = {
-          'image': {
-            'url': _0x43a4ee[0].thumbnail
-          },
-          'caption': "HANS-MD-MUC-DOWNLODER\n\n © HANS TECH"
-        };
-        await _0x1dc8a0.sendMessage(_0x80c678, _0x232b70, {
-          'quoted': _0xfb7714
-        });
-        await _0x1dc8a0.sendMessage(_0x80c678, {
-          'audio': {
-            'url': _0x26f57c
-          },
-          'mimetype': "audio/mp4"
-        }, {
-          'quoted': _0xfb7714
-        });
-        _0x19492f("*Downloded Successfully ✅*");
-      } else {
-        _0x19492f("Failed to download audio. Please try again later.");
-      }
-    } else {
-      _0x19492f("No audio found.");
+    const _0x2ffce4 = await yts(_0x538ab4.join(" "));
+    const _0x522668 = _0x2ffce4.videos;
+    if (_0x522668.length === 0) {
+      return _0x47720b("𝙎𝙤𝙧𝙧𝙮 𝙉𝙤 𝙖𝙪𝙙𝙞𝙤 𝙛𝙤𝙪𝙣𝙙.");
     }
-  } catch (_0x45d554) {
-    console.error("Error from API:", _0x45d554);
-    _0x19492f("An error occurred while searching or downloading the audio.");
+    const _0x1bad4b = _0x522668[0].url;
+    const _0x3467d0 = await fetch(BaseUrl + "/api/download/ytmp3?url=" + encodeURIComponent(_0x1bad4b) + "&apikey=" + giftedapikey);
+    const _0x748034 = await _0x3467d0.json();
+    if (_0x748034.status === 200 && _0x748034.success) {
+      const _0x5c9669 = _0x748034.result.download_url;
+      await _0x49043b.sendMessage(_0x1cb756, {
+        'image': {
+          'url': _0x522668[0].thumbnail
+        },
+        'caption': "╔═━━━━━════━━━━─➳\n║ ╔ *𝘿𝙊𝙒𝙉𝙇𝙊𝘿𝙀𝙍* 』\n║ *𝘽𝙤𝙩 𝙣𝙖𝙢𝙚 ; 𝙃𝘼𝙉𝙎-𝙈𝘿 2025* \n║ *𝙊𝙬𝙣𝙚𝙧; 𝙃𝘼𝙉𝙎𝙏𝙕* \n╚══━━━━════━━━━─➳"
+      }, {
+        'quoted': _0x999bbc
+      });
+      await _0x49043b.sendMessage(_0x1cb756, {
+        'audio': {
+          'url': _0x5c9669
+        },
+        'mimetype': "audio/mp4"
+      }, {
+        'quoted': _0x999bbc
+      });
+      _0x47720b("𝙔𝙤𝙪𝙧-𝙨𝙤𝙣𝙜-𝙙𝙤𝙬𝙣𝙡𝙤𝙖𝙙-𝙨𝙪𝙘𝙘𝙚𝙨𝙨𝙛𝙪𝙡𝙡𝙮");
+    } else {
+      _0x47720b("𝙁𝙖𝙞𝙡𝙚𝙙 𝙩𝙤 𝙙𝙤𝙬𝙣𝙡𝙤𝙙 𝙥𝙡𝙖𝙨𝙚 𝙩𝙧𝙮 𝙖𝙜𝙖𝙣 𝙖𝙛𝙚𝙬 𝙨𝙚𝙘....");
+    }
+  } catch (_0x30df29) {
+    console.error("Error:", _0x30df29);
+    _0x47720b("An error occurred while processing your request.");
+  }
+});
+zokou({
+  'nomCom': "song",
+  'categorie': "Download",
+  'reaction': '✨'
+}, async (_0x3b6f05, _0x264109, _0x4e9748) => {
+  const {
+    ms: _0x410d5c,
+    repondre: _0x493e98,
+    arg: _0x58a684
+  } = _0x4e9748;
+  if (!_0x58a684[0]) {
+    return _0x493e98("𝙋𝙡𝙖𝙨𝙚 𝙚𝙣𝙩𝙚𝙧 𝙮𝙤𝙪𝙧 𝙨𝙤𝙣𝙜 𝙣𝙖𝙢𝙚.");
+  }
+  try {
+    const _0x44a64d = await yts(_0x58a684.join(" "));
+    const _0x22be7d = _0x44a64d.videos;
+    if (_0x22be7d.length === 0) {
+      return _0x493e98("𝙎𝙤𝙧𝙧𝙮 𝙉𝙤 𝙖𝙪𝙙𝙞𝙤 𝙛𝙤𝙪𝙣𝙙.");
+    }
+    const _0x24cfb4 = _0x22be7d[0].url;
+    const _0x192d05 = await fetch(BaseUrl + "/api/download/ytmp3?url=" + encodeURIComponent(_0x24cfb4) + "&apikey=" + giftedapikey);
+    const _0x33269d = await _0x192d05.json();
+    if (_0x33269d.status === 200 && _0x33269d.success) {
+      const _0x4c45e7 = _0x33269d.result.download_url;
+      await _0x264109.sendMessage(_0x3b6f05, {
+        'image': {
+          'url': _0x22be7d[0].thumbnail
+        },
+        'caption': "╔═━━━━━════━━━━─➳\n║ ╔ *𝘿𝙊𝙒𝙉𝙇𝙊𝘿𝙀𝙍* 』\n║ *𝘽𝙤𝙩 𝙣𝙖𝙢𝙚 ; 𝙃𝘼𝙉𝙎-𝙈𝘿 2025* \n║ *𝙊𝙬𝙣𝙚𝙧; 𝙃𝘼𝙉𝙎𝙏𝙕* \n╚══━━━━════━━━━─➳"
+      }, {
+        'quoted': _0x410d5c
+      });
+      await _0x264109.sendMessage(_0x3b6f05, {
+        'audio': {
+          'url': _0x4c45e7
+        },
+        'mimetype': "audio/mp4"
+      }, {
+        'quoted': _0x410d5c
+      });
+      _0x493e98("𝙔𝙤𝙪𝙧-𝙨𝙤𝙣𝙜-𝙙𝙤𝙬𝙣𝙡𝙤𝙖𝙙-𝙨𝙪𝙘𝙘𝙚𝙨𝙨𝙛𝙪𝙡𝙡𝙮");
+    } else {
+      _0x493e98("𝙁𝙖𝙞𝙡𝙚𝙙 𝙩𝙤 𝙙𝙤𝙬𝙣𝙡𝙤𝙙 𝙥𝙡𝙖𝙨𝙚 𝙩𝙧𝙮 𝙖𝙜𝙖𝙣 𝙖𝙛𝙚𝙬 𝙨𝙚𝙘....");
+    }
+  } catch (_0xba12b4) {
+    console.error("Error:", _0xba12b4);
+    _0x493e98("An error occurred while processing your request.");
   }
 });
