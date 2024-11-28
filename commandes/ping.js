10e86633d606234bec5ec18fc9272da66a139fdf
FELIXTZ <hanstzgamestech@gmail.com>
2024-11-28 20:32:58 +0300

    Update ping.js

--- commandes/ping.js
+++ commandes/ping.js
@@ -1,7 +1,5 @@
 const { zokou } = require("../framework/zokou");
-const moment = require("moment-timezone");
 const { default: axios } = require('axios');
-//const conf = require('../set');
 
 zokou({ 
   nomCom: 'ping',
@@ -11,14 +9,13 @@ zokou({
   fromMe: 'true', 
 },
 async (dest, zk, commandeOptions) => {
-  const { ms, arg, repondre } = commandeOptions;
-  const start = new Date().getTime();
+  const { repondre } = commandeOptions;
   
   // Generate a random ping time between 50ms and 500ms for fun
   const randomPing = Math.floor(Math.random() * (500 - 50 + 1)) + 50;
 
-  // Owner's photo URL (the URL you provided)
-  const ownerPhotoUrl = "https://files.catbox.moe/l1i9o4.jpg";
+  // Owner's photo URL
+  const ownerPhotoUrl = "https://files.catbox.moe/l1i9o4.jpg";  // Updated image URL
 
   // Send the random ping time with a photo
   await repondre(`
@@ -28,6 +25,8 @@ async (dest, zk, commandeOptions) => {
     ![Owner's photo](${ownerPhotoUrl})
   `);
   
+  // Send the ping calculation response
+  const start = new Date().getTime();
   const end = new Date().getTime();
   await zokou.sendMessage('*Pong!*\n ```' + (end - start) + '``` *ms*');
 }
