7d9eda6ccbaa679534b9e82500dd46775b286b7a
FELIXTZ <hanstzgamestech@gmail.com>
2024-11-28 17:52:19 +0300

    Update ping.js

--- commandes/ping.js
+++ commandes/ping.js
@@ -1,24 +1,30 @@
 const { zokou } = require("../framework/zokou");
 const moment = require("moment-timezone");
 const { default: axios } = require('axios');
-//const conf = require('../set');
 
-
-zokou({ nomCom: 'ping',
+zokou(
+  {
+    nomCom: 'ping',
     desc: 'To check ping',
     Categorie: 'General',
-    reaction: '🚀', 
-    fromMe: 'true', 
-
-       
+    reaction: '🚀',
+    fromMe: 'true',
   },
   async (dest, zk, commandeOptions) => {
-    const { ms, arg, repondre } = commandeOptions;
-    const { start} = new Date().getTime()
-    return repondre('*ʜɪ ✌️ ʜᴀɴs-ᴍᴅ-sᴘᴇᴇᴅ-ɪs*\n ```' +0700+ '``` *ᴍ/s*') 
-    const { end } = new Date().getTime()
-    await zok.sendMessage('*Pong!*\n ```' + (end - start) + '``` *ms*')
+    const { arg, repondre } = commandeOptions;
+    const start = new Date().getTime();
+
+    // Define the bot image URL
+    const botImageUrl = 'https://files.catbox.moe/l1i9o4.jpg';
+
+    // Measure the end time
+    const end = new Date().getTime();
+    const ping = end - start;
+
+    // Send response with bot image and ping info
+    await zk.sendMessage(dest, {
+      caption: `*ʜɪ ✌️ ʜᴀɴs-ᴍᴅ-sᴘᴇᴇᴅ-ɪs*\n\`\`\`${ping}\`\`\` *ms*`,
+      image: { url: botImageUrl }, // Attach the bot image
+    });
   }
-)
-
-
+);
