9a15ac4d6601d331d9a037dd4cbf182c114ddef4
FELIXTZ <hanstzgamestech@gmail.com>
2024-12-03 00:29:32 +0300

    Update 3b.js

--- commandes/3b.js
+++ commandes/3b.js
@@ -148,7 +148,7 @@ zokou({
           'video': {
             'url': _0x1edad2
           },
-          'caption': "*HI YOUR VIDEO SUCCESSFULY DOWNLOADED BY HANS TZ*",
+          'caption': "*HI ✌️AM HANS-MD YOUR VIDEO SUCCESSFULY DOWNLOADED BY HANS TZ*",
           'mimetype': "video/mp4"
         }, {
           'quoted': _0x38072b
